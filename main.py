a1c8a8399fd34d339c198c08c4e1bf58c4934f28
Máté Bendegúz Kovács <20370225+kmb5@users.noreply.github.com>
2021-09-25 09:51:51 +0200

    added descriptions to python file

--- main.py
+++ main.py
@@ -6,10 +6,13 @@
 ###############################
 
 # YOUR ASSIGNMENT:
+# For all the exercises below, test your function by calling it 
+# (if it has parameters, test it with different arguments).
+
 ### Exercise 1
-# Write a function to check if a number is in a given range 
-# Example: Given the range 4 to 10, check if 5 is included
-# The expected result is True
+# Write a function to check if a number is in a given range.
+# (Hint: you need 3 parameters, 2 for defining the range, 1 for the number you want to check.)
+# Example: Given the range 4 to 10, check if 5 is included. The expected result is True.
 # You could also return a phrase like, “The number 5 is included in the range”
 
 # ------ ADD YOUR CODE BELOW: ------
@@ -20,6 +23,7 @@
 
 ### Exercise 2
 # Create a function that will help us what to take with us by giving the weather
+# (So the function has a parameter called weather.)
 # Examples:
 # If it’s sunny, your function will return “Take sunglasses”
 # If it’s raining, your function will return “Take umbrella”
@@ -40,8 +44,43 @@
 # Create a function that produces a random integer integer between (0,100) and asks for user to guess the number.
 # If the user guessed the number correctly, it prints "That's right! You guessed it! Congrats"
 # If the user could not guess it right, it prints "I'm sorry, but that's not correct."
+# (hint: this function should not have any parameters)
 
 # ------ ADD YOUR CODE BELOW: ------
 
+
+
+
+
+
+### Bonus Exercise:
+
+# This exercise is totally optional, but you can do it if you are up for a challenge.
+# Remember the Exercise 2 from week 3 homework? Let's do it using an input() function this time.
+# Assign a variable called occupation using the input() function, using a description to print,
+# like "What is your occupation?".
+# Similarly assign a variable number_of_children by asking a question
+# (like "Give number of children") to the user using the input() function.
+# For each child, ask the age of the child to the user similarly using the input() function, 
+# and calculate the total benefit amount.
+
+# The description of Exercise 2 from last week:
+
+# Situation: Citizens apply for financial benefits from the government whose policy is to
+# grant government employee benefits for only 2 of his children, 1.000Euro for each child
+# if he/she is less than or equal to 20 years old.
+
+# Write a set of codes using the conditional statements (if, else, elif) that checks
+# the following conditions:
+# - If citizen's occupation is a government employee, check if he has kids eligible for benefits,
+# calculate the total benefit amount
+# - If citizen's occupation is an entrepreneur, output the message "You make sure you pay your tax!"
+# - If citizen has other occupation, output the message
+# "This facility is only for government employees!
+# 
+# At the end output the message "Total benefit availed: " and total benefit amount
+
+
+# ------ ADD YOUR CODE BELOW: ------
 
 
